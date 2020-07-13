f509b083ec1fedf3954af0daec470ac395a60cee
Katharina Sick <katharina.sick@hotmail.com>
2020-07-13 13:06:38 +0200

    layer 3 finally done :)

--- decode_single_layer.kts
+++ decode_single_layer.kts
@@ -28,10 +28,9 @@ fun File.decodeLayer2(resultFileName: String) {
                 val parity = (byte and 0b00000001u).toInt()
 
                 var numberOfOnes = 0
-                var shiftedByte = byte
+                val byteToShift = byte
                 for (i in 0 until 7) {
-                    shiftedByte = shiftedByte.shiftRight()
-                    numberOfOnes += (shiftedByte and 0b00000001u).toInt()
+                    numberOfOnes += (byteToShift.shiftRight() and 0b00000001u).toInt()
                 }
 
                 parity % 2 == numberOfOnes % 2
@@ -44,6 +43,45 @@ fun File.decodeLayer2(resultFileName: String) {
             .map { binaryByte ->
                 Integer.parseInt(binaryByte, 2).toUByte()
             }
+            .toText()
+            .saveToFile(resultFileName)
+}
+
+fun File.decodeLayer3(resultFileName: String) {
+    val bytes = decodeAscii85()
+    val key = UByteArray(32)
+
+    // Start of file:
+    "==[ Layer 4/6: " // key[0-14]
+            .toCharArray()
+            .map { it.toInt().toUByte() }
+            .withIndex()
+            .forEach {
+                key.set(it.index, it.value xor bytes[it.index])
+            }
+
+    // One line has 60 characters
+    // "...===" now only goes until char 46 -> char 47 to 59 have to be '=', followed by two new lines
+    "=============\n\n" // // key[14-29]
+            .toCharArray()
+            .map { it.toInt().toUByte() }
+            .withIndex()
+            .forEach {
+                key.set(15 + it.index, it.value xor bytes[32 + 15 + it.index])
+            }
+
+    // Gets clear when looking at the decrypted file from above
+    " ]" // key[30-31]
+            .toCharArray()
+            .map { it.toInt().toUByte() }
+            .withIndex()
+            .forEach {
+                key.set(30 + it.index, it.value xor bytes[30 + it.index])
+            }
+
+    bytes
+            .withIndex()
+            .map { it.value xor key.get(it.index % 32) }
             .toText()
             .saveToFile(resultFileName)
 }
